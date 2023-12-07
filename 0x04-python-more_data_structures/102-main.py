2091d80b9065a2008f22de9fa0ca78f05d5e1c37
NIHAL <nihaleloudiki16@gmail.com>
2023-12-07 04:43:58 -0800

    handle task

--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -15,17 +15,3 @@ new_dict = complex_delete(a_dictionary, 'c_is_fun')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
-
-guillaume@ubuntu:~/0x04$ ./102-main.py
-ids: [1, 2, 3]
-track: Low
---
-ids: [1, 2, 3]
-track: Low
---
---
-ids: [1, 2, 3]
-track: Low
---
-ids: [1, 2, 3]
-track: Low
