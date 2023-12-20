75648c1d24cd055c4368c648f71ccb273c4d9be9
NIHAL <nihaleloudiki16@gmail.com>
2023-12-19 16:35:09 -0800

    handle task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is Class squere """
+
+
+class Square:
+    """  This class allows you to create and manipulate square objects """
+
+    def __init__(self, size):
+        """ Initialize a new Square
+            args:
+                size (int): The size for new Saquare
+        """
+        self._Square__size = size
