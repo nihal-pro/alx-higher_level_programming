28e329976c8d14b058633f394a8f6ce1eca91236
NIHAL <nihaleloudiki16@gmail.com>
2024-01-08 17:45:59 -0800

    handle tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains a class that defines a rectangle.
+"""
+
+
+class BaseGeometry:
+    """Defines a BaseGeometry"""
+
+    def area(self):
+        """Method: raises an Exception with the message
+            area() is not implemented
+        """
+        raise Exception('area() is not implemented')
