47c79c6bc7d3414a29561d57716b5bee558540ba
NIHAL <nihaleloudiki16@gmail.com>
2024-01-16 00:51:24 +0100

    handle tasks

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Create Rectangle that inherits from Base"""
+from models.base import Base
+
+class Rectangle(Base):
+    """Create class Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Create object of the class Rectangle"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        """Getter and setter for Width"""
+        @property
+        def width(self):
+            return self.__width
+        
+        @width.setter
+        def width(self, value):
+            if value <= 0:
+                raise ValueError("Width must be > 0")
+            self.__width = value
+
+        """Getter and setter for height"""
+        @property
+        def height(self):
+            return self.__height
+        
+        @height.setter
+        def height(self, value):
+            if value <= 0:
+                raise ValueError("height must be > 0")
+            self.__height = value
+
+        """Getter and setter for x"""
+        @property
+        def x(self):
+            return self.__x
+        
+        @width.setter
+        def x(self, value):
+            self.__x = value
+        
+        """Getter and setter for y"""
+        @property
+        def y(self):
+            return self.__y
+        
+        @width.setter
+        def y(self, value):
+            self.__y = value
