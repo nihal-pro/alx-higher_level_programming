6918bb976a0718a85d2b0fbb662cbac4cde82949
NIHAL <nihaleloudiki16@gmail.com>
2024-01-16 22:11:31 +0100

    handle tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         """retrieve The width of Rectangle.
         Return: an integer of the width."""
         if type(value) is not int:
-            raise TypeError('width must be an integer') 
+            raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError('width must be > 0')
         self.__width = value
@@ -39,7 +39,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError('height must be an integer') 
+            raise TypeError('height must be an integer')
         if value <= 0:
             raise ValueError('height must be > 0')
         self.__height = value
@@ -53,7 +53,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if type(value) is not int:
-            raise TypeError('x must be an integer') 
+            raise TypeError('x must be an integer')
         if value < 0:
             raise ValueError('x must be >= 0')
         self.__x = value
@@ -67,7 +67,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if type(value) is not int:
-            raise TypeError('y must be an integer') 
+            raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
