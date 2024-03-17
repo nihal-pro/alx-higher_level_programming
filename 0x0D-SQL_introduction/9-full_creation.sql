7461aed010ce2624626516022caffe607b957a4e
NIHAL <nihaleloudiki16@gmail.com>
2024-03-17 18:00:19 +0000

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- Full create in the database hbtn_0c_0 in your MySQL server
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, "John", 10), (3, "Bob", 14), (2, "Alx", 3), (4, "George", 8);
