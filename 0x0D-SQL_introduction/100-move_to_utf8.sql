cdf1d23c598d9f131550dcb7bc3f881576e8786b
NIHAL <nihaleloudiki16@gmail.com>
2024-03-17 22:25:40 +0000

    advanced

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Write a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+-- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p 
+USE hbtn_0c_0
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
