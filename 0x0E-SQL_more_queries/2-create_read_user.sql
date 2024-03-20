b5e50c2d95d4789daacabc763e96be6ca5320e7e
NIHAL <nihaleloudiki16@gmail.com>
2024-03-20 17:28:02 +0000

    Create user&database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create user and database in localhost
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
