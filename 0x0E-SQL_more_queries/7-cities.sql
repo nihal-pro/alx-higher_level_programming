a642645828db9eb85a91df8a3fbeeac089c3bd68
NIHAL <nihaleloudiki16@gmail.com>
2024-03-20 22:39:03 +0000

    Foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Foreign Key database and table in localhost
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    state_id INT NOT NULL, 
+    name VARCHAR(256) NOT NULL, 
+    FOREIGN KEY (state_id) REFERENCES states(id));
