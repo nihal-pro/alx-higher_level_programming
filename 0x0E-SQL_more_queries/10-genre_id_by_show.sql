94aad2c2d4d675a3d6b8984b94aa20ba54a8b36a
NIHAL <nihaleloudiki16@gmail.com>
2024-03-21 00:14:26 +0000

    Foreign key&Primary key

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Genre ID by show in localhost
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres
+ON  tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
