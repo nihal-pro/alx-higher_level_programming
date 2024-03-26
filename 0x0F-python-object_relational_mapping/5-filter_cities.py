55ce6d4e928d83318fac8c60e7a761a77dcd9848
NIHAL <nihaleloudiki16@gmail.com>
2024-03-26 18:30:24 +0000

    without ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+''' write one that is safe from MySQL injections! '''
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM cities WHERE state_id IN\
+                (SELECT id FROM states WHERE name = %s)\
+                ORDER BY cities.id", (state_name, ))
+    query_rows = cur.fetchall()
+    for index, row in enumerate(query_rows):
+        if index < len(query_rows) - 1:
+            sep = ', '
+            end = ''
+        else:
+            sep = ''
+            end = '\n'
+        print(row[0] + sep, end=end)
+    cur.close()
+    conn.close()
