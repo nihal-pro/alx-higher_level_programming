e3346b06a571e6d768488e89ddd445e37e3a81c5
NIHAL <nihaleloudiki16@gmail.com>
2024-03-27 22:37:56 +0000

    with ORM

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''Write a script that lists all State objects with a from the database'''
+
+import sys
+import sqlalchemy
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id)
+    .filter(State.name.like('%a%')).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
