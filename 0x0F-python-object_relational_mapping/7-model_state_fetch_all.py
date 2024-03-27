2ad0cc3f7cd94039b25e2d068fe3d0e62994c459
NIHAL <nihaleloudiki16@gmail.com>
2024-03-27 18:34:43 +0000

    with ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,6 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'.format
         (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
