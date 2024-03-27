e693c4905894ba776ff4a8786c210d291800f790
NIHAL <nihaleloudiki16@gmail.com>
2024-03-27 18:07:35 +0000

    with ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Write a python file that contains the class definition'''
+
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Create class table """
+    __tablename__ = "states"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
