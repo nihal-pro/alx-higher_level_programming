77c70820d35535a35570220a5665e5f95fb41ba0
NIHAL <nihaleloudiki16@gmail.com>
2024-03-28 00:12:56 +0000

    with ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''Write a script that deletes all State objects with a
+name containing the letter a from table in the database'''
+
+import sys
+import sqlalchemy
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state, city in session.query(State, City).\
+            filter(State.id == City.state_id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''Write a python file that contains the class definition'''
+
+import sqlalchemy
+from model_state import Base
+
+
+class City(Base):
+    """ Create class table """
+    __tablename__ = "cities"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    state_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.sql.schema.ForeignKey('states.id'), nullable=False)
