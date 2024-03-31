c4d610dedaae77888b17e6c3ec7e3dedc78cd37e
NIHAL <nihaleloudiki16@gmail.com>
2024-03-31 23:54:42 +0000

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+
+if (Args.length > 1) {
+  console.log('Arguments found');
+} else if (Args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
