caf6adac3a413f314d1afc978e97ef4a254ac1e6
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 01:00:33 +0000

    js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+const Num = parseInt(Args[0]);
+
+function Factorial (Number) {
+  if (Number === 1) {
+    return 1;
+  }
+  return Number * Factorial(Number - 1);
+}
+
+if (isNaN(Num)) {
+  console.log('1');
+} else {
+  console.log(Factorial(Num));
+}
