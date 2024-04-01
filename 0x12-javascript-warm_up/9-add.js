7d7b3a2b6c0c470dd4e0130153a0408389e425c1
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 00:59:02 +0000

    js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+const number1 = parseInt(Args[0]);
+const number2 = parseInt(Args[1]);
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+if (isNaN(number1) || isNaN(number2)) {
+  console.log('NaN');
+} else {
+  add(number1, number2);
+}
