69a76094e07205f1f11147909f016fcaabd2e5fc
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:28:27 +0000

    API

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import requests
+    with requests.get('https://alx-intranet.hbtn.io/status') as req:
+        response = req.text
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
