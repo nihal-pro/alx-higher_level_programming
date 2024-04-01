c0d4b2ca5665cd1641ab939978c7910d2ef30c2e
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 01:46:13 +0000

    js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const _Square = require('./5-square');
+
+module.exports = class Square extends _Square {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
