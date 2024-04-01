c03819195657d699a5922731d6a4081cae35aca3
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:13:47 +0000

    API

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""import module"""
+
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
+    with request.urlopen(argv[1]) as requests:
+        if requests.getcode() == 200:
+            # print(requests.headers)
+            print(requests.headers['X-Request-Id'])
