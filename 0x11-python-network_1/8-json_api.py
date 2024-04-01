98bf491c54109e5434bf6bdcf75da992dd461c1f
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:32:17 +0000

    API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""import model"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    request = requests.get(argv[1])
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
