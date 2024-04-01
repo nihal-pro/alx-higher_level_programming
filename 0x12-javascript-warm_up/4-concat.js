b197d116b4ad1dd9ebb5f88bbbca987b1eac63af
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 00:49:33 +0000

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,3 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+console.log(Args[0], 'is', Args[1]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+const number = parseInt(Args[0]);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', number);
+}
