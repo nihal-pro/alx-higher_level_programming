e741e089d6bf67f4d0f0706ce38b0aff62ef5a5b
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:24:02 +0000

    API

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""import module"""
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    data = parse.urlencode({'email': argv[2]}).encode('utf-8')
+    # print(data)
+    with request.urlopen(argv[1], data) as requests:
+        if requests.getcode() == 200:
+            content = requests.read().decode('utf-8')
+            print(content)
