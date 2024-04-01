e5086316075e15fefcc343d626e9633d79e23744
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 01:51:49 +0000

    js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+    const NewList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      NewList.push(list[i]);
+    }
+    return NewList;
+  };
