ccee7d39abc894d2a618ae1389eddf24176ba371
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:26:19 +0000

    API

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""import module"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request, error
+    try:
+        with request.urlopen(argv[1]) as resp:
+            if resp.getcode() == 200:
+                content = resp.read().decode('utf-8')
+                print(content)
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
