d85985547f63b0c71893667349c10cd84201be00
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 01:59:30 +0000

    js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const dc = require('./101-data').dict;
+
+const New = {};
+for (const key in dc) {
+  if (New[dc[key]] === undefined) {
+    New[dc[key]] = [];
+  }
+  New[dc[key]].push(key);
+}
+console.log(New);
