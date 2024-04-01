b8e37147841144dc30086a07d1c7f0e90a4d679e
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:33:00 +0000

    API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""import module"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    auth = (argv[1], argv[2])
+    request = requests.get("https://api.github.com/user", auth=auth)
+    print(request.json().get("id"))
