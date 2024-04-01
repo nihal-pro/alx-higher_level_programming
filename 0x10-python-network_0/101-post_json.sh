f6233863594738a6edc3d4f86609e483b84aa5f9
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:05:10 +0000

    API

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request with json
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
