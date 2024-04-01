fbca89c533bafeaaba717c10489bc2dbc06d39a0
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 03:33:39 +0000

    API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""import module"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
