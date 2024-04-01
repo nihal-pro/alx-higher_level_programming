725d6cbcc6c970ccd94975f76a58ed834a913201
NIHAL <nihaleloudiki16@gmail.com>
2024-04-01 01:47:54 +0000

    js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for (const se of list) {
+      if (se === searchElement) {
+        count++;
+      }
+    }
+    return count;
+  };
