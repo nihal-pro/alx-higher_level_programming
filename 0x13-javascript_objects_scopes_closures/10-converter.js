cb00f463f94125e7b349a4fe9dd9d538a4ec3ca6
NIHAL <nihaleloudiki16@gmail.com>
2024-04-05 02:45:26 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
 exports.converter = function (base) {
-    return function (number) {
-      return number.toString(base);
-    };
-  };
+  function theConverter (number) {
+    return (number.toString(base));
+  }
+  return (theConverter);
+};
