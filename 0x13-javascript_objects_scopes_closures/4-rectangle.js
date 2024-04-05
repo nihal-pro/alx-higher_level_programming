a2db16b4567e9788010ebe489663f98fe62ffd2a
NIHAL <nihaleloudiki16@gmail.com>
2024-04-05 02:41:31 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,25 +1,32 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
       }
+      console.log(line);
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
-    }
-  
-    rotate () {
-      const tmp = this.width;
-      this.width = this.height;
-      this.height = tmp;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  };
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+    return (this.width, this.height);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+    return (this.width, this.height);
+  }
+};
