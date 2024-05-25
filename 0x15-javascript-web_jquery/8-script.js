abaa92694a7d665ec23f1dd79a317769b783ce54
NIHAL <nihaleloudiki16@gmail.com>
2024-05-25 15:24:49 +0100

    get data

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,4 @@
-#!/usr/bin/node
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data){
+    $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title} </li>`
+    ));
+});
