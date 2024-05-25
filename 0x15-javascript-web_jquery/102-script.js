584a733526ee9919e87780b9923ce40510a2e146
NIHAL <nihaleloudiki16@gmail.com>
2024-05-25 16:04:03 +0100

    hello

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+$('document').ready(function (){
+    const api = 'https://www.fourtonfish.com/hellosalut/hello/';
+    $('INPUT#btn_translate').click(function() {
+        $.get(url + $.param({ lang: $('INPUT#language_code').val () }), function (data) {
+            $('DIV#hello').html(data.hello);
+        });
+    });
+});
